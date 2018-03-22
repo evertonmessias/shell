1bf9a20e465c931eb51e137de1299789dab875c5
evertonmessias <everton.messias@gmail.com>
2018-03-21 21:51:50 -0300

    21-03-2018_21:51:50

--- commitar.sh
+++ commitar.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-clear
-rm .gitignore
-echo "*********** REMOVIDO GITIGNORE OK ******************"
-git add *
-echo "**************** GIT ADD OK ************************"
+git config credential.helper store
+find ./ -iname .gitignore -exec rm {} \;
+echo "##### GITIGNORE EXCLUIDO ==> OK #####"
+git add .
+echo "##### GIT ADD ==> OK #####"
 data=`date +%d-%m-%Y_%H:%M:%S`
 git commit -m $data
-echo "*************** GIT COMMIT OK **********************"
+echo "##### COMMIT ==> OK #####"
 git push
+echo "##### PUSH ==> OK #####"
